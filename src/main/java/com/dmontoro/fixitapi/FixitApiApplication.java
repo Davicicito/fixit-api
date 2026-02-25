fd686247d861ab1695cbd1d77754b01532cdc225
davicicito <davidmontoroguillen@gmail.com>
2026-02-25 19:34:59 +0100

    primer commit

---
+++ src/main/java/com/dmontoro/fixitapi/FixitApiApplication.java
@@ -0,0 +1,13 @@
+package com.dmontoro.fixitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixitApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dmontoro/fixitapi/FixitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmontoro.fixitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
